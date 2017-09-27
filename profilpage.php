e3c25b14e6f063994d0bea58a910649831ab2abd
Patrickmn86 <32276640+Patrickmn86@users.noreply.github.com>
2017-09-27 12:09:49 +0200

    Add files via upload
    Second Menu - GONE

--- profilpage.php
+++ profilpage.php
@@ -128,21 +128,6 @@ padding-bottom: 50px;
 
 
 
- <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#"><img id="logo" src="images/logo.png"></a>
-    </div>
-    <ul class="nav navbar-nav">
-      
-      <li><a href="#">Upload</a></li>
-      <li><a href="#">SignOut</a></li>
-      
-    </ul>
-  </div>
-</nav>
-
-
 
 
 <div class="container-fluid">
