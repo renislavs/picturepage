310659a82f6780358d293f7d38ea9680f947ff01
Marianne Nielsen <haffemn@gmail.com>
2017-09-29 08:42:25 +0200

    Bit of clean-up (head)

--- profilpage.php
+++ profilpage.php
@@ -11,24 +11,21 @@
 <html lang="en">
   <head>
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <meta name="description" content="Group 7">
+    <meta name="author" content="Upload and vote on pictures">
+    <link rel="icon" href="./img/favicon.ico">
 
     <title>ProfilPage</title>
 
     <!-- Bootstrap core CSS -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom styles for this template -->
     <link href="starter-template.css" rel="stylesheet">
     <link href="./css/mystyle.css" rel="stylesheet">
@@ -38,16 +35,15 @@
   margin-left: 10px;
 }
 body {
-    background-image: none!important;
-    background-color: #f2f2f2;
+    background-image: url("img/luca.jpg");
 }
   .line {
 width: 80px;
 height: 4px;
-background-color: #019fb3;
+
   }
   h1, h2, h3, h4, p {
-    color: #686868;
+    color: white;
   }
   .color{
     background-color:#019fb3;
@@ -60,9 +56,7 @@ background-color: #019fb3;
   margin-bottom: 0px;
   margin-top: 8px;
 }
-.example {
-border: 1px lightgrey solid;
-}
+
   </style>
   </head>
 
@@ -72,15 +66,12 @@ border: 1px lightgrey solid;
     include './includes/menu.inc.php';
 ?>
 
-<div class="container">
-<div class="row">
+<div class="container-fluid">
+<div class="row rowleft">
 
   <div class="col-sm-12 col-md-6">
-  <h3>Add image to your library</h3>
-       
-        <!--button style="margin-bottom: 30px;"type="button" class="btn btn-default dropdown-toggle">
-         Browse
-          </button-->    
+  <h3 class="title1">Add image to your library</h3>
+          
     <form action="imgdb.php" method="post" id='deform' enctype="multipart/form-data"> 
         <input type="hidden" name="MAX_FILE_SIZE" value="256000"/>
         <input type='file' id='bild' name='img' style="margin-bottom: 30px;" class="btn btn-default dropdown-toggle" />
@@ -89,18 +80,18 @@ border: 1px lightgrey solid;
         <input type="text" name="caption">
 
         <p class="Uploadtext">Story</p>
-        <input rows="4" cols="50" type="text" name="story"></input>
+        <input rows="4" cols="50" type="text" name="story">
 
         <p class="Uploadtext">Tags</p>
         <input type="text" name="tags"><br>
-
+        <p>By uploading your picture, you accept that PicturePage can use it.</p>
         <button style="margin-bottom: 30px; margin-top: 10px;" type="submit" class="btn btn-default dropdown-toggle">
          Add image to Library
           </button>  
 <?php        
 
         if (ISSET($_SESSION["error"])) {
-            print("<p>".$_SESSION["error"]."</p>");
+            print("<p style='color:red'>".$_SESSION["error"]."</p>");
         }
 ?>                
     </form>
@@ -108,17 +99,18 @@ border: 1px lightgrey solid;
 
 </div>
 
-<div class="row">
+    <div class="conatiner-fluid container-lib">
+<div class="row rowleft">
 
 <div class="col-sm-12 col-md-12">
-     <h1 class="title">Your Library</h1>
+     <h1 class="title title2">Your Library</h1>
      <div class="line"></div>
        
 </div>
 </div>
 <br><br>
 
-<div class="row rowmar">
+<div class="row rowleft">
 <?php
     require_once './includes/DbP.inc.php';
     require_once './includes/DbH.inc.php';
@@ -136,59 +128,41 @@ border: 1px lightgrey solid;
         $q->bindValue(':email', Authentication::getEmail());
         $q->execute();
         
+        $images = array();
+        while ($out = $q->fetch()) {
+            $g = new Photo($out['caption'], $out['credit'], $out['id'], $out['imagedata'], $out['mimetype'], $out['story'], $out['tags']);
+            $g->setVotes($out['votes']);
+            array_push($images, $g);
+        }
     } catch(PDOException $e) {
-        $_SESSION['error'] = "Could not create img (".$e->getMessage().")";
+        $_SESSION['error'] = "Could not get img (".$e->getMessage().")";
       //  header('Location: ./profilPage.php?'.$e->getMessage());
         die("Reading failed.<br />".$sql."<br />".$e->getMessage());
     } catch (Exception $e) {
-        $_SESSION['error'] = "Could not create img (".$e->getMessage().")";
+        $_SESSION['error'] = "Could not get img (".$e->getMessage().")";
       //  header('Location: ./profilPage.php?'.$e->getMessage());
         die("Reading failed.<br />".$sql."<br />".$e->getMessage());
-    }
+    }    
     
-    $images = array();
-    while ($out = $q->fetch()) {
-        $g = new Photo($out['caption'], $out['credit'], $out['id'], $out['imagedata'], $out['mimetype'], $out['story'], $out['tags']);
-        $g->setVotes($out['votes']);
-        array_push($images, $g);
-    }
-
     foreach($images as $image){
-        print('<div class="col-xs-12 col-md-2">');
+        print("<div class='col-xs-12 col-md-2'>\n");
         print($image->getCaption()."<br />");
-        echo sprintf('
-                    <a class="thumbnail" href="getImage.php?id=%s" title="%s" alt="%s">
-                        <img src="getImage.php?id=%s" class="img-fluid" />
-                    </a>
-                ', 
+        echo sprintf("<a class='thumbnail' href='getImage.php?id=%s' title='%s' alt='%s'>
+                        ".$image."</a>\n", 
                 $image->getId(),
                 $image->getStory(),
                 $image->getCaption(),
-                $image->getId()                           
+                $image->getId()
             );
-        print($image->getVotes()." votes");
-        print("</div>");
+        print($image->getVotes()." votes\n");
+        print("</div>\n");
     }
    
 ?>
           
    </div>
-
-<br><br>
+ </div>
 
 </div><!-- /.container -->
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"></script>
-    <script src="./js/vendor/popper.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="./js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
-
   </body>
 </html>
