84cdeb5fd74882b8064e4c0116412129ba3f975c
Marianne Nielsen <haffemn@gmail.com>
2017-09-29 08:39:31 +0200

    Comment on environment

--- includes/DbP.inc.php
+++ includes/DbP.inc.php
@@ -1,5 +1,6 @@
 <?php
-abstract class DbP {
+abstract class DbP { 
+    // Change according to host (production/test environment)
     const DBHOST = 'web1.mari59dg.iba-abakomp.dk';
     const DBUSER = 'c1dbu1';
     const USERPWD = 'x0syRr0PNA';
